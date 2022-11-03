195a34ec394aee5f13200984eaeaf9b896420079
ip07 <78528552+its-pablo@users.noreply.github.com>
2022-11-02 18:35:49 -0700

    Make login refresh explorer

--- libmultiuser.js
+++ libmultiuser.js
@@ -3,6 +3,7 @@ function loginUser (username) {
     le._path.home = "/a/users/" + username + "/";
     le._path.desktop = "/a/users/" + username + "/desktop/";
     le._settings = $store.get("users/" + username + "/settings.json");
+    $explorer.refresh();
 }
 function createUser (username) {
   localStorage.setItem("users/"+username+"/settings.json", localStorage["settings.json"]);

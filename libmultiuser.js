e80eba5f38ab84db9e43d7c1b33ae1f5c3868d79
MrPigeontheCoder <105377999+MrPigeontheCoder@users.noreply.github.com>
2023-06-04 17:36:10 +0200

    Added some code to make an images and documents folder Currently working on making them appear on the user's desktop

--- libmultiuser.js
+++ libmultiuser.js
@@ -8,6 +8,8 @@ function loginUser (username) {
 function createUser (username) {
   localStorage.setItem("users/"+username+"/settings.json", localStorage["settings.json"]);
   localStorage.setItem("users/"+username+"/desktop/welcome.txt", "This is the desktop for your user, whatever you put into here will only be for this user");
+  localStorage.setItem("users/"+username+"/documents/welcome.txt", "This is the documents folder for your user, it's only yours and you can store lots of documents in here");
+  localStorage.setItem("users/"+username+"/images/welcome_to_the_imagery.txt", "This is the images folder for your user, no one else can see it (unless they dig into /a/), feel free to put your images in here");
 }
 /* 
   How to use this:
